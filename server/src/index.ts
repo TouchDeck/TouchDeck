523395c52208efea1c38b0c967cbde4888747e00
Luca Scalzotto <luca@scorpiac.com>
2020-08-17 18:02:00 +0200

    Set up obs websocket

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello, world!');
+import ObsSocket from './obsSocket';
+
+console.log('Starting server...');
+
+async function start() {
+  const obs = new ObsSocket();
+  await obs.connect();
+}
+
+start().catch(err => console.error('An unhandled error occurred:', err));
---
+++ server/src/obsSocket.ts
@@ -0,0 +1,29 @@
+import OBSWebSocket from 'obs-websocket-js';
+
+export interface VersionResult {
+  'obs-websocket-version': string;
+  'obs-studio-version': string;
+}
+
+export default class ObsSocket {
+  private readonly obs: OBSWebSocket;
+
+  public constructor() {
+    this.obs = new OBSWebSocket();
+  }
+
+  public async connect(): Promise<void> {
+    await this.obs.connect();
+    const version = await this.getVersion();
+    console.log(`Connected to OBS version ${version['obs-studio-version']}, websocket plugin version ${version['obs-websocket-version']}`);
+  }
+
+  public async getVersion(): Promise<VersionResult> {
+    return this.obs.send('GetVersion');
+  }
+
+  public async setScene(scene: string): Promise<void> {
+    return this.obs.send('SetCurrentScene', { 'scene-name': scene });
+  }
+}
+
