fa7490c14f0ee74e03606da6c05fb34c67cc3f04
Luca Scalzotto <luca@scorpiac.com>
2020-09-09 14:21:21 +0200

    Basic connect page

--- web/src/components/Icon.tsx
+++ web/src/components/Icon.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 export interface Props {
   icon: string;
+  style?: 's' | 'b';
 }
 
-const Icon: React.FC<Props> = ({ icon }) => <i className={`fas fa-${icon}`} />;
+const Icon: React.FC<Props> = ({ icon, style }) => (
+  <i className={`fa${style || 's'} fa-${icon}`} />
+);
 
 export default Icon;
--- web/src/pages/ConnectAgentPage.tsx
+++ web/src/pages/ConnectAgentPage.tsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useGlobalState } from '../state/appState';
+import Icon from '../components/Icon';
+import Agent from '../api/Agent';
 
 const ConnectAgentPage: React.FC = () => {
-  return <>Connect to an agent!</>;
+  const [, dispatch] = useGlobalState();
+
+  const [connectInput, setConnectInput] = useState('');
+  const [agentsList, setAgentsList] = useState<string[]>();
+
+  useEffect(() => {
+    // Load the agents from the server.
+    // TODO
+    setAgentsList(['http://localhost:3000', '127.0.0.1', '192.168.0.10']);
+  }, []);
+
+  const connectToAgent = useCallback(
+    (address) => {
+      dispatch({
+        type: 'agentConnecting',
+      });
+      const agent = new Agent(address);
+      // TODO: test if agent is valid.
+      dispatch({
+        type: 'agentConnected',
+        agent,
+      });
+    },
+    [dispatch]
+  );
+
+  return (
+    <div className="connect-agent">
+      <main>
+        <h2>Connect to an agent</h2>
+
+        <h3>Select an agent from the list (todo):</h3>
+        <div className="agents-list">
+          {!agentsList && <Icon icon="spinner fa-3x fa-pulse" />}
+          {agentsList &&
+            agentsList.map((agent) => (
+              <div key={agent} onClick={() => connectToAgent(agent)}>
+                <Icon icon="windows fa-2x" style="b" /> {agent}
+              </div>
+            ))}
+        </div>
+
+        <h3>Or enter the IP address:</h3>
+        <input
+          placeholder="localhost:3000"
+          value={connectInput}
+          onChange={(e) => setConnectInput(e.currentTarget.value)}
+        />
+        <button
+          disabled={!connectInput}
+          onClick={() => connectToAgent(connectInput)}
+        >
+          Connect
+        </button>
+      </main>
+    </div>
+  );
 };
 
 export default ConnectAgentPage;
