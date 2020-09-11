252d48a6d4c0525e395d3aac930240687ebe6456
Luca Scalzotto <luca@scorpiac.com>
2020-09-11 16:52:40 +0200

    Keep discovery agents in memory for limited time

--- discovery/src/agents.ts
+++ discovery/src/agents.ts
@@ -1,29 +1,50 @@
 import { Request, Response } from 'express';
+import { KEEP_AGENT_TIME } from './constants';
 
 export type Platform = 'windows' | 'linux' | 'apple' | 'other';
 
 export interface AgentInfo {
   address: string;
   platform: Platform;
+  removalTimeout: NodeJS.Timeout;
 }
 
 const agents: { [publicIp: string]: AgentInfo[] } = {};
 
+function removeAgent(reqIp: string, address: string): void {
+  agents[reqIp] = (agents[reqIp] || []).filter((a) => a.address !== address);
+}
+
 export function getAgents(req: Request, res: Response): void {
-  res.json(agents[req.ip] || []);
+  const response = (agents[req.ip] || []).map((agent) => ({
+    address: agent.address,
+    platform: agent.platform,
+  }));
+  res.json(response);
 }
 
 export function registerAgent(req: Request, res: Response): void {
   const info: AgentInfo = req.body;
+  const currentAgents = agents[req.ip] || [];
 
-  // Get the current agents, removing any that have the same address as the new one.
-  const existingAgents = (agents[req.ip] || []).filter(
-    (a) => a.address !== info.address
+  // If there is an agent with the same address, clear the removal timeout and remove it.
+  const sameAddressAgent = currentAgents.find(
+    (a) => a.address === info.address
+  );
+  if (sameAddressAgent) {
+    clearTimeout(sameAddressAgent.removalTimeout);
+  }
+  const newAgents = currentAgents.filter((a) => a.address !== info.address);
+
+  // Set a timeout for removing the new agent.
+  info.removalTimeout = setTimeout(
+    () => removeAgent(req.ip, info.address),
+    KEEP_AGENT_TIME * 1000
   );
 
   // Add the new agent to the list, store it.
-  existingAgents.push(info);
-  agents[req.ip] = existingAgents;
+  newAgents.push(info);
+  agents[req.ip] = newAgents;
 
   getAgents(req, res);
 }
--- discovery/src/constants.ts
+++ discovery/src/constants.ts
@@ -4,3 +4,8 @@ function envOrDefault(param: string, def: string): string {
 }
 
 export const PORT = parseInt(envOrDefault('PORT', '5000'), 10);
+
+export const KEEP_AGENT_TIME = parseInt(
+  envOrDefault('KEEP_AGENT_TIME', '600'),
+  10
+);
