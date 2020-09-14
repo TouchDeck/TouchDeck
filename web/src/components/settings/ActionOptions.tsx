dcc5170aacac0c9789f00f2ad0fd97d13600ff0e
Luca Scalzotto <luca@scorpiac.com>
2020-09-14 21:06:07 +0200

    Add ActionOptions dropdown, store button settings in state

---
+++ web/src/components/settings/ActionOptions.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { useConnectedAgent } from '../../state/appState';
+import ActionOption from '../../model/ActionOption';
+
+export interface Props {
+  actionType: string;
+  onChange: (type: string) => void;
+}
+
+const ActionOptions: React.FC<Props> = ({ actionType, onChange }) => {
+  const { actionOptions } = useConnectedAgent();
+
+  const [optionGroups, setOptionGroups] = useState<{
+    [category: string]: ActionOption[];
+  }>({});
+
+  useEffect(() => {
+    // All category groups.
+    // This is initialized with the empty category so that appears as the top of the list.
+    const groups: { [category: string]: ActionOption[] } = { '': [] };
+
+    // Sort all actions into groups.
+    actionOptions.forEach((option) => {
+      const group = groups[option.category] || [];
+      group.push(option);
+      groups[option.category] = group;
+    });
+    setOptionGroups(groups);
+  }, [actionOptions]);
+
+  return (
+    <select
+      value={actionType}
+      onChange={(e) => onChange(e.currentTarget.value)}
+    >
+      {Object.entries(optionGroups).map(([category, options]) => (
+        <optgroup key={category} label={category}>
+          {options.map((option) => (
+            <option key={option.id} value={option.id}>
+              {option.name}
+            </option>
+          ))}
+        </optgroup>
+      ))}
+    </select>
+  );
+};
+
+export default ActionOptions;
--- web/src/components/settings/NormalButtonSettings.tsx
+++ web/src/components/settings/NormalButtonSettings.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NormalButtonConfig } from '../../model/configuration/ButtonConfig';
 import Rows from '../Rows';
+import ActionOptions from './ActionOptions';
 
 export interface Props {
   button: NormalButtonConfig;
 }
 
 const NormalButtonSettings: React.FC<Props> = ({ button }) => {
+  const [name, setName] = useState(button.name);
+  const [text, setText] = useState(button.text);
+  const [textColor, setTextColor] = useState(button.textColor);
+  const [backgroundColor, setBackgroundColor] = useState(
+    button.backgroundColor
+  );
+  const [actionType, setActionType] = useState(button.action.type);
+
+  useEffect(() => {
+    setName(button.name);
+    setText(button.text);
+    setTextColor(button.textColor);
+    setBackgroundColor(button.backgroundColor);
+    setActionType(button.action.type);
+  }, [button]);
+
   return (
     <div>
       <h3>Button</h3>
@@ -14,25 +31,37 @@ const NormalButtonSettings: React.FC<Props> = ({ button }) => {
         <div>
           <span>Name</span>
           <span>
-            <input value={button.name} />
+            <input
+              value={name || ''}
+              onChange={(e) => setName(e.currentTarget.value)}
+            />
           </span>
         </div>
         <div>
           <span>Text</span>
           <span>
-            <input value={button.text} />
+            <input
+              value={text || ''}
+              onChange={(e) => setText(e.currentTarget.value)}
+            />
           </span>
         </div>
         <div>
           <span>Text color</span>
           <span>
-            <input value={button.textColor} />
+            <input
+              value={textColor || ''}
+              onChange={(e) => setTextColor(e.currentTarget.value)}
+            />
           </span>
         </div>
         <div>
           <span>Background color</span>
           <span>
-            <input value={button.backgroundColor} />
+            <input
+              value={backgroundColor || ''}
+              onChange={(e) => setBackgroundColor(e.currentTarget.value)}
+            />
           </span>
         </div>
       </Rows>
@@ -41,7 +70,12 @@ const NormalButtonSettings: React.FC<Props> = ({ button }) => {
       <Rows>
         <div>
           <span>Type</span>
-          <span>{button.action.type}</span>
+          <span>
+            <ActionOptions
+              actionType={actionType}
+              onChange={(newAction) => setActionType(newAction)}
+            />
+          </span>
         </div>
       </Rows>
     </div>
