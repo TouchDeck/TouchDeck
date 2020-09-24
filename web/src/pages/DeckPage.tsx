9b9713658bb6f455a1e3be27fbea8217b9d9767a
Luca Scalzotto <luca@scorpiac.com>
2020-09-24 23:33:27 +0200

    Start adding button editing to layout, remove unused components

--- web/src/pages/DeckPage.tsx
+++ web/src/pages/DeckPage.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonGrid from '../components/buttons/ButtonGrid';
+import ButtonList from '../components/buttons/ButtonList';
+import { ButtonConfig } from '../model/configuration/ButtonConfig';
+import ButtonSettings from '../components/settings/ButtonSettings';
 
 export interface Props {
   editing?: boolean;
 }
 
-const DeckPage: React.FC<Props> = ({ editing }) => (
-  <ButtonGrid rowCount={3} columnCount={5} editing={editing} />
-);
+const DeckPage: React.FC<Props> = ({ editing }) => {
+  const [selectedButton, setSelectedButton] = useState<ButtonConfig>();
+
+  return (
+    <main className="deck">
+      {editing && (
+        <div className="edit-panel">
+          <ButtonList onClickButton={setSelectedButton} />
+          {selectedButton && <ButtonSettings button={selectedButton} />}
+        </div>
+      )}
+      <ButtonGrid rowCount={3} columnCount={5} editing={editing} />
+    </main>
+  );
+};
 
 export default DeckPage;
