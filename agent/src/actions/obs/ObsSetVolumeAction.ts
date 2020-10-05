58f064b3d46eb27a6b9e6c11c1805342abbae1fc
Luca Scalzotto <luca@scorpiac.com>
2020-10-06 01:05:59 +0200

    Add useDecibel param to ObsSetVolume

--- agent/src/actions/obs/ObsSetVolumeAction.ts
+++ agent/src/actions/obs/ObsSetVolumeAction.ts
@@ -7,11 +7,14 @@ export default class ObsSetVolumeAction implements Action {
 
   public async invoke(
     @param('source') source: string,
-    @param('volume') volume: number
+    @param('volume') volume: number,
+    @param('use decibel') useDecibel: boolean
   ): Promise<void> {
     await (await this.obs.getSocket()).send('SetVolume', {
       source,
       volume,
+      // @ts-ignore useDecibel is a valid property, but does not exist in the type definition.
+      useDecibel,
     });
   }
 }
