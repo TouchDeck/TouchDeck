db73d89dc9738d7b812dd0ddf1be7941f2c02e24
Luca Scalzotto <luca@scorpiac.com>
2020-10-21 18:34:01 +0200

    Create button settings modal

---
+++ web/src/components/buttons/EditButtonModal.tsx
@@ -0,0 +1,86 @@
+import React, { useCallback, useState } from 'react';
+import { ButtonConfig } from '../../model/configuration/ButtonConfig';
+import Modal from '../Modal';
+import Button from '../Button';
+import TextInput from '../input/TextInput';
+import ButtonStyleSettings from '../settings/ButtonStyleSettings';
+import ButtonActionSettings from '../settings/ButtonActionSettings';
+import { useConnectedAgent, useGlobalState } from '../../state/appState';
+import ButtonGroup from '../ButtonGroup';
+
+export interface Props {
+  button: ButtonConfig;
+  onClose: () => void;
+}
+
+const EditButtonModal: React.FC<Props> = ({ button, onClose }) => {
+  const [, dispatch] = useGlobalState();
+  const { agent } = useConnectedAgent();
+  const [updates, setUpdates] = useState<ButtonConfig>({ ...button });
+
+  const onSave = useCallback(async () => {
+    dispatch({ type: 'configLoading' });
+    const newConfig = await agent.upsertButton({
+      ...updates,
+      id: button.id,
+    });
+    dispatch({ type: 'configLoaded', config: newConfig });
+    onClose();
+  }, [agent, button.id, dispatch, updates, onClose]);
+
+  return (
+    <Modal className="edit-button" active onClose={onClose}>
+      <TextInput
+        className="name"
+        value={updates.name}
+        onChange={(name) => setUpdates((prevState) => ({ ...prevState, name }))}
+      />
+      {'style' in updates && (
+        <ButtonStyleSettings
+          buttonStyle={updates.style}
+          onChange={(style) =>
+            setUpdates((prevState) => ({ ...prevState, style }))
+          }
+        />
+      )}
+      {'action' in updates && (
+        <ButtonActionSettings
+          action={updates.action}
+          onChange={(action) =>
+            setUpdates((prevState) => ({ ...prevState, action }))
+          }
+        />
+      )}
+      {'trueStyle' in updates && (
+        <ButtonStyleSettings
+          buttonStyle={updates.trueStyle}
+          onChange={(style) =>
+            setUpdates((prevState) => ({
+              ...prevState,
+              trueStyle: style,
+            }))
+          }
+        />
+      )}
+      {'falseStyle' in updates && (
+        <ButtonStyleSettings
+          buttonStyle={updates.falseStyle}
+          onChange={(style) =>
+            setUpdates((prevState) => ({
+              ...prevState,
+              falseStyle: style,
+            }))
+          }
+        />
+      )}
+      <ButtonGroup className="actions">
+        <Button onClick={onSave} positive icon="save">
+          Save
+        </Button>
+        <Button onClick={onClose}>Cancel</Button>
+      </ButtonGroup>
+    </Modal>
+  );
+};
+
+export default EditButtonModal;
--- web/src/pages/DeckPage.tsx
+++ web/src/pages/DeckPage.tsx
@@ -4,6 +4,7 @@ import ButtonList from '../components/buttons/ButtonList';
 import { ButtonConfig } from '../model/configuration/ButtonConfig';
 import ButtonSettings from '../components/settings/ButtonSettings';
 import newButton from '../util/newButton';
+import EditButtonModal from '../components/buttons/EditButtonModal';
 
 export interface Props {
   editing?: boolean;
@@ -28,6 +29,12 @@ const DeckPage: React.FC<Props> = ({ editing }) => {
           />
           {selectedButton && (
             <ButtonSettings
+              button={selectedButton}
+              onClose={() => setSelectedButton(undefined)}
+            />
+          )}
+          {selectedButton && (
+            <EditButtonModal
               button={selectedButton}
               onClose={() => setSelectedButton(undefined)}
             />
