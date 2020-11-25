9b47fb4bc0eaab1533f7ee1f41627929b7526587
Luca Scalzotto <luca@scorpiac.com>
2020-11-25 16:31:18 +0100

    Add toggle full screen button to layout

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Icon from './Icon';
 import Menu from './Menu';
@@ -7,6 +7,17 @@ const Layout: React.FC = ({ children }) => {
   const location = useLocation();
   // If the pathname is longer than 1 ('/'), default the menu to open.
   const [showMenu, setShowMenu] = useState(location.pathname.length > 1);
+
+  const [isFullscreen, setIsFullscreen] = useState(
+    !!document.fullscreenElement
+  );
+
+  useEffect(() => {
+    // Subscribe to the fullscreen change event, so we can change the icon.
+    document.addEventListener('fullscreenchange', () =>
+      setIsFullscreen(!!document.fullscreenElement)
+    );
+  }, []);
 
   return (
     <div className="layout">
@@ -21,6 +32,20 @@ const Layout: React.FC = ({ children }) => {
         </Link>
       )}
       <Menu open={showMenu} onClose={() => setShowMenu(false)} />
+      <Icon
+        className="toggle-fullscreen"
+        icon={isFullscreen ? 'compress-alt' : 'expand-alt'}
+        size={2}
+        onClick={() => {
+          // Toggle fullscreen.
+          // No need to await any of these promises since we don't handle the errors anyway.
+          if (document.fullscreenElement) {
+            document.exitFullscreen();
+          } else {
+            document.documentElement.requestFullscreen();
+          }
+        }}
+      />
       {children}
     </div>
   );
