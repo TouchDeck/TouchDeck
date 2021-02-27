1ca10eb6c8f5bfe8dc4cb21e304ca55f4c001dac
Luca Scalzotto <luca@scorpiac.com>
2021-02-27 16:21:17 +0100

    Improve checkbox looks (#22)

--- web/src/components/input/CheckboxInput.tsx
+++ web/src/components/input/CheckboxInput.tsx
@@ -1,4 +1,5 @@
 import React, { InputHTMLAttributes } from 'react';
+import { classNames } from '../../util/classNames';
 
 export interface Props
   extends Omit<
@@ -9,9 +10,20 @@ export interface Props
 }
 
 export const CheckboxInput: React.FC<Props> = (props) => (
-  <input
-    {...props}
-    type="checkbox"
-    onChange={(e) => props.onChange(e.currentTarget.checked)}
-  />
+  <div className={classNames(['checkbox', props.checked && 'checked'])}>
+    <input
+      {...props}
+      type="checkbox"
+      onChange={(e) => props.onChange(e.currentTarget.checked)}
+    />
+    <span
+      onClick={() => props.onChange(!props.checked)}
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === ' ') {
+          props.onChange(!props.checked);
+        }
+      }}
+    />
+  </div>
 );
