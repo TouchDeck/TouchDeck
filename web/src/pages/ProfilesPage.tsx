9ff20a7906d8e01dd59b7bb7db75b96c731d5163
Luca Scalzotto <luca@scorpiac.com>
2021-03-01 23:36:00 +0100

    Start on profile properties

--- web/src/pages/ProfilesPage.tsx
+++ web/src/pages/ProfilesPage.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useConnectedAgent } from '../state/appState';
 import { SimpleList } from '../components/SimpleList';
 import { Profile } from 'touchdeck-model';
+import { ProfileProperties } from '../components/ProfileProperties';
 
 export const ProfilesPage: React.FC = () => {
   const { config } = useConnectedAgent();
+  const [selectedProfile, setSelectedProfile] = useState<Profile>();
+
+  const deleteProfile = useCallback(async (profile: Profile) => {
+    // TODO
+    setSelectedProfile(undefined);
+  }, []);
 
   return (
     <main className="profiles-page config-page">
@@ -12,8 +19,16 @@ export const ProfilesPage: React.FC = () => {
         searchPlaceholder="Search profiles..."
         entries={config.profiles}
         entryName={(p) => p.name}
-        onClickEntry={() => undefined}
+        onClickEntry={setSelectedProfile}
       />
+      {selectedProfile && (
+        <ProfileProperties
+          profile={selectedProfile}
+          onDelete={() => deleteProfile(selectedProfile)}
+          onClose={() => setSelectedProfile(undefined)}
+          isDefaultProfile={selectedProfile.id === config.defaultProfile}
+        />
+      )}
     </main>
   );
 };
