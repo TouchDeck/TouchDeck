51b234bde84241aa5d26d6938d4c8ba7f17cb5ef
Luca Scalzotto <luca@scorpiac.com>
2021-03-01 22:55:37 +0100

    Add profile list to page, SimpleList component

---
+++ web/src/components/SimpleList.tsx
@@ -0,0 +1,43 @@
+import React, { ReactNode, useLayoutEffect, useState } from 'react';
+import { List } from './List';
+import { searchEntries } from '../util/searchEntries';
+
+export interface Props<T> {
+  children?: ReactNode;
+  entries: T[];
+  entryName: (entry: T) => string;
+  onClickEntry: (entry: T) => void;
+  searchPlaceholder: string;
+}
+
+export function SimpleList<T>({
+  children,
+  entries,
+  entryName,
+  onClickEntry,
+  searchPlaceholder,
+}: Props<T>) {
+  const [showEntries, setShowEntries] = useState(entries);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useLayoutEffect(() => {
+    setShowEntries(searchEntries(entries, searchTerm, entryName));
+  }, [entries, searchTerm, entryName]);
+
+  return (
+    <List
+      searchTerm={searchTerm}
+      onSearchTermChange={setSearchTerm}
+      searchPlaceholder={searchPlaceholder}
+    >
+      {children}
+      <div className="entries">
+        {showEntries.map((entry, i) => (
+          <div key={i} className="entry" onClick={() => onClickEntry(entry)}>
+            <span className="name">{entryName(entry)}</span>
+          </div>
+        ))}
+      </div>
+    </List>
+  );
+}
--- web/src/pages/ProfilesPage.tsx
+++ web/src/pages/ProfilesPage.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { List } from '../components/List';
+import { useConnectedAgent } from '../state/appState';
+import { SimpleList } from '../components/SimpleList';
+import { Profile } from 'touchdeck-model';
 
 export const ProfilesPage: React.FC = () => {
+  const { config } = useConnectedAgent();
+
   return (
     <main className="profiles-page config-page">
-      <List />
+      <SimpleList<Profile>
+        searchPlaceholder="Search profiles..."
+        entries={config.profiles}
+        entryName={(p) => p.name}
+        onClickEntry={() => undefined}
+      />
     </main>
   );
 };
